eafc0e3187af6320010ea5584504aa6bac710764
sayek ariz <88825949+leakshark@users.noreply.github.com>
2023-10-13 16:29:12 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function toggleMenu() {
+    var menu = document.getElementById("menuItems");
+    if (menu.style.display === "block") {
+        menu.style.display = "none";
+    } else {
+        menu.style.display = "block";
+    }
+}
